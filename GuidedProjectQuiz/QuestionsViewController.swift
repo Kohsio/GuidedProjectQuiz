aa87606e5eae001ec70cc039892fa08fa24975c5
Kohsio <kohsio@alexiss-Mac.local>
2018-08-23 04:20:50 -0700

    end of quiz

--- GuidedProjectQuiz/QuestionsViewController.swift
+++ GuidedProjectQuiz/QuestionsViewController.swift
@@ -139,7 +139,7 @@ class QuestionsViewController: UIViewController {
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == resultSegueID{
+        if segue.identifier == resultSegueID {
             let resultsViewController = segue.destination as! ResultsViewController
             resultsViewController.responses = answersChosen
         }
--- GuidedProjectQuiz/ResultsViewController.swift
+++ GuidedProjectQuiz/ResultsViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class ResultsViewController: UIViewController {
 
+    
+    @IBOutlet weak var resultAnswerLabel: UILabel!
+    @IBOutlet weak var resultDefinitionLabel: UILabel!
+    
     var responses: [Answer]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        calculatePersonalityResult()
 
         // Do any additional setup after loading the view.
     }
@@ -23,7 +28,19 @@ class ResultsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
+    func calculatePersonalityResult(){
+        var frequencyOfAnswer: [AnimalType: Int] = [:]
+        let responseTypes = responses.map {$0.type}
+        
+        for response in responseTypes {
+            frequencyOfAnswer[response] = (frequencyOfAnswer[response] ?? 0) + 1
+        }
+        let frequentAnswerSorted = frequencyOfAnswer.sorted(by: {(pair1, pair2) -> Bool in return pair1.value > pair2.value})
+        let mostCommonAnswer = frequencyOfAnswer.sorted(by: {$0.1 > $1.1} ).first!.key
+        
+        resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+        resultDefinitionLabel.text = mostCommonAnswer.definition
+    }
     
 
     /*
